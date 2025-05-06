d324e962d7a34d032cb8a08992c296dd8b5471e1
GITHUB_ALEXIA_2027 <177985127+alexia2027@users.noreply.github.com>
2025-05-05 19:40:44 -0600

    next

--- src/controller_fsm.vhd
+++ src/controller_fsm.vhd
@@ -38,8 +38,31 @@ entity controller_fsm is
 end controller_fsm;
 
 architecture FSM of controller_fsm is
-
+    type state_type is (S0, S1, S2, S3);
+    signal state : state_type;
 begin
-
-
+    process(i_reset, i_adv)
+    begin
+        if i_reset = '1' then
+            state <= S0;
+        elsif rising_edge(i_adv) then
+            case state is
+                when S0 => 
+                    state <= S1;
+                when S1 => 
+                    state <= S2;
+                when S2 => 
+                    state <= S3;
+                when S3 => 
+                    state <= S0;
+            end case;
+        end if;
+    end process;
+    
+    -- Output logic
+    with state select
+        o_cycle <= "0001" when S0,  -- Clear display
+                   "0010" when S1,  -- Store 1st operand
+                   "0100" when S2,  -- Store 2nd operand
+                   "1000" when S3;  -- Display result
 end FSM;
